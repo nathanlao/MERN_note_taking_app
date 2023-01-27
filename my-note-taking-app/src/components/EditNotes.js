75c4df8d6bb396a592d0634ee97e1ef81553e33e
nathanlao <gla93@sfu.ca>
2023-01-26 20:45:12 -0800

    Added eventlistener for submit form and added onChange for input changes

--- my-note-taking-app/src/components/EditNotes.js
+++ my-note-taking-app/src/components/EditNotes.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function EditNotes() {
+
+    const [note, setNote] = useState("")
+
+    function handleSubmit(event) {
+        // Prevent the page refresh
+        event.preventDefault()
+        
+        console.log(note)
+
+        // Clear the textarea
+        setNote("")
+    }
+    
+    function handleChange(event) {
+        setNote(event.target.value)
+    }
+
     return (
-        <form className="form-container">
+        <form className="form-container" onSubmit={handleSubmit}>
             <textarea 
                 className="form-textarea"
                 placeholder="Type your note's title here..."
+                onChange={handleChange}
+                // Controlled component
+                value={note}
                 />
             <button className="form-save-note">SAVE NOTE</button>
         </form>
