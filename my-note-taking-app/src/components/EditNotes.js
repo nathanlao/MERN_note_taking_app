d937864c10e8d9a609ae17cdcf445cfcfb73c740
nathanlao <gla93@sfu.ca>
2023-01-26 20:26:32 -0800

    Added placeholder value for textArea

--- my-note-taking-app/src/components/EditNotes.js
+++ my-note-taking-app/src/components/EditNotes.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function EditNotes() {
     return (
         <form className="form-container">
-            <textarea className="form-textarea"/>
+            <textarea 
+                className="form-textarea"
+                placeholder="Type your note's title here..."
+                />
             <button className="form-save-note">SAVE NOTE</button>
         </form>
     )
