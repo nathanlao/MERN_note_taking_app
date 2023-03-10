3ab9a239fdde0993034ff19653de3f0a643a70a3
nathanlao <gla93@sfu.ca>
2023-03-10 15:57:56 -0800

    Define the schema for Notes model

---
+++ server/NotesModel.js
@@ -0,0 +1,32 @@
+// Create a Note scehma and model
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const NoteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    timeLastModified: {
+        type: Date,
+        default: Date.now
+    },
+    timeCreated: {
+        type: Date,
+        default: Date.now
+    },
+    color: {
+        type: String,
+        default: "white"
+    },
+    priority: {
+        type: Number,
+        default: 0
+    }
+})
+
+module.exports = mongoose.model('Notes', NoteSchema)
