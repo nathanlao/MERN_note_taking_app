59f056fb0bdc2c3374bc0c946f86a04c81ac7eda
nathanlao <gla93@sfu.ca>
2023-03-10 21:12:33 -0800

    Code refractor: handleNotesSubmit in EditNote.js

--- my-note-taking-app/src/App.js
+++ my-note-taking-app/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react"
 import EditNotes from "./components/EditNotes"
 import ListNotes from "./components/ListNotes"
-import swal from "sweetalert"
-import { nanoid } from "nanoid"
 
 export default function App() {
     // localStorage.clear()
@@ -19,45 +17,9 @@ export default function App() {
         localStorage.setItem("myNotes", JSON.stringify(notes))
     }, [notes])
     
-    function handleNotesSubmit(newNote) {
-        if (newNote.trim().length === 0) {
-            swal({
-                title: "Warning",
-                text: "Please type down your notes before saving...",
-                icon: "warning",
-                button: "OK"
-            });
-        } else {
-
-            // split the newNote with title and body
-            const newLineIndex = newNote.indexOf("\n")
-            let title = ""
-            let body = ""
-
-            // if no newline found
-            if(newLineIndex === -1) {
-                title = newNote
-                body = ""
-            } else {
-                title = newNote.slice(0, newLineIndex)
-                body = newNote.slice(newLineIndex + 1)
-            }
-
-            // Give the newNote a nanoid
-            const newNoteObject = {
-                id: nanoid(),
-                title: title,
-                body: body
-            }
-            setNotes(prevNotes => {
-                return [...prevNotes, newNoteObject]
-            })
-        }
-    }
-
     return (
         <main>
-            <EditNotes handleNotesSubmit={handleNotesSubmit}/>
+            <EditNotes setNotes={setNotes}/>
             <ListNotes notes={notes}/>
         </main>
     )
--- my-note-taking-app/src/components/EditNotes.js
+++ my-note-taking-app/src/components/EditNotes.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import swal from "sweetalert"
+import { nanoid } from "nanoid"
 
-export default function EditNotes(props) {
+export default function EditNotes({ setNotes }) {
 
     const [note, setNote] = useState("")
 
@@ -8,14 +10,47 @@ export default function EditNotes(props) {
         // Prevent the page refresh
         event.preventDefault()
         
-        props.handleNotesSubmit(note)
+        if (note.trim().length === 0) {
+            swal({
+                title: "Warning",
+                text: "Please type down your notes before saving...",
+                icon: "warning",
+                button: "OK"
+            });
+        } else {
+
+            // split the newNote with title and body
+            const newLineIndex = note.indexOf("\n")
+            let title = ""
+            let body = ""
+
+            // if no newline found
+            if(newLineIndex === -1) {
+                title = note
+                body = ""
+            } else {
+                title = note.slice(0, newLineIndex)
+                body = note.slice(newLineIndex + 1)
+            }
+
+            // Give the newNote a nanoid
+            const newNoteObject = {
+                id: nanoid(),
+                title: title,
+                body: body
+            }
+            setNotes(prevNotes => {
+                return [...prevNotes, newNoteObject]
+            })
+        }
 
         // Clear the textarea
         setNote("")
     }
     
     function handleChange(event) {
-        setNote(event.target.value)
+        const { value } = event.target
+        setNote(value)
     }
 
     return (
@@ -24,7 +59,7 @@ export default function EditNotes(props) {
                 className="form-textarea"
                 placeholder="Title your note here and separate content with new line for clear organization..."
                 onChange={handleChange}
-                // Controlled component
+                // Controlled component (controlled by react state)
                 value={note}
                 name={note}
                 />
