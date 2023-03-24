1902a013e7d20c62210ebd5e7e6431438bdd6362
nathanlao <gla93@sfu.ca>
2023-03-24 15:27:55 -0700

    Style the list of notes, added created time

--- my-note-taking-app/src/pages/ListNotes.jsx
+++ my-note-taking-app/src/pages/ListNotes.jsx
@@ -1,41 +1,45 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
-import { Grid, Typography, ListItem, ListItemText, ListItemIcon, IconButton } from "@mui/material"
+import { Grid, List, Typography, ListItem, ListItemText, ListItemIcon, IconButton, Divider } from "@mui/material"
 import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import moment from "moment"
 
 export default function ListNotes({ notes }) {
 
+    console.log(notes)
+
     // Map over note element with <li> into component
-    const noteElements = notes.map((note, index) => {
+    const noteElements = notes.map((note) => {
         return  (
-            <ListItem key={note._id} secondaryAction={
-                    <IconButton edge="end" aria-label="delete">
-                        <DeleteForeverOutlinedIcon />
-                    </IconButton>
-                }
-                sx={{ py: 0.5 }}
-            >
-                <ListItemIcon>
-                    <EventNoteOutlinedIcon />
-                </ListItemIcon>
-                <Link to={`/${note._id}`} >
-                    <ListItemText primary={note.title} sx={{ marginLeft: -2 }}/>
-                </Link>
-            </ListItem>
+            <List key={note._id} >
+                <ListItem secondaryAction={
+                        <IconButton edge="end" aria-label="delete">
+                            <DeleteForeverOutlinedIcon />
+                        </IconButton>
+                    }
+                    sx={{ py: 0.5 }}
+                    className="list-notes"
+                >
+                    <ListItemIcon>
+                        <EventNoteOutlinedIcon />
+                    </ListItemIcon>
+                    <Link to={`/${note._id}`} >
+                        <ListItemText 
+                            primary={note.title} sx={{ marginLeft: -2 }}
+                            secondary={`Created at: ${moment(note.timeCreated).startOf('ss').fromNow()}`}
+                        />
+                    </Link>
+                </ListItem>
+                <Divider component="li" />
+            </List>
         )
     })
 
     return (
-        <section className="section-container">
-            <Typography sx={{ mt: 4, mb: 4 }} variant="h5" component="div">SAVED NOTES:</Typography> 
-            <Grid container spacing={2}>
-                <Grid item xs={6} md={4}>
-                    <Grid container spacing={2} direction="column">
-                        {noteElements}
-                    </Grid>
-                </Grid>
-            </Grid>
+        <section className="list-notes-container">
+            <Typography sx={{ mt: 0, mb: 2 }} variant="h5" component="div">SAVED NOTES:</Typography> 
+            {noteElements}
         </section>
     )
 }
