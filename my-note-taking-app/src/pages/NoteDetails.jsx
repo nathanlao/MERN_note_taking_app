cb537a07005d05e96d3af76386fb6e9044e944af
nathanlao <gla93@sfu.ca>
2023-03-24 14:26:32 -0700

    Fetch note details, using dialog for pop up window

--- my-note-taking-app/src/pages/NoteDetails.jsx
+++ my-note-taking-app/src/pages/NoteDetails.jsx
@@ -1,34 +1,93 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { Dialog, DialogContent, DialogTitle, DialogActions, Button } from "@mui/material";
+import { useParams, Link } from "react-router-dom";
+import { ListItem, ListItemText, ListItemIcon, Divider, 
+    Dialog, DialogContent, DialogTitle, DialogActions, Button, Typography } from "@mui/material";
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import moment from "moment"
+import axios from "axios";
 
 export default function NoteDetails() {
     // Open the modal depends on id on the path
     const [open, setOpen] = useState(false)
+    const [noteDetail, setNoteDetail] = useState([])
     const { id } = useParams()
 
     useEffect(() => {
         if (id) {
             setOpen(true)
         }
-    }, [id])
+
+        if (open) {
+            async function getNoteDetails() {
+                try {
+                    const response = await axios.get(`http://localhost:3001/api/notes/${id}`)
+                    if (response.status !== 200) {
+                        // eslint-disable-next-line no-throw-literal
+                        throw {
+                            message: "Failed to fetch note details", 
+                            statusText: response.statusText,
+                            status: response.status
+                        }
+                    }
+                    console.log("Fetching note details from db")
+                    const data = response.data
+                    setNoteDetail(data)
+                    console.log(data)
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+            getNoteDetails()
+        }
+    }, [id, open])
 
     function handleClose() {
         setOpen(false)
     }
 
+    let noteBodyEl
+    if (noteDetail?.body) {
+        noteBodyEl = noteDetail?.body.split("\n").map((line, index) => {
+            // Avoid user enter extra "\n"
+            if (line === "") {
+                return null;
+            }
+            return (
+                <ListItem key={index} style={{ pt: 0, pb: 1.5, pl: 0 }}>
+                    <ListItemIcon>
+                        <ArrowCircleRightIcon fontSize="large" />
+                    </ListItemIcon>
+                    <ListItemText primary={line} />
+                </ListItem>
+            )
+        })
+    }
+    
     return (
         <>
-            {/* <h1>
-                Notes modal will be here with id: {id}
-            </h1> */}
-            <Dialog open={open}>
-                <DialogTitle>Set backup account</DialogTitle>
-                <DialogContent>
-                
+            <Dialog open={open} onClose={handleClose} PaperProps={{
+                    style: {
+                        backgroundColor: `${noteDetail.color}`,
+                        color: "#333333",
+                        borderRadius: "10px",
+                        overflow: "hidden",
+                    },
+                }}
+            >
+                <DialogTitle className="dialog-title">
+                    <Typography variant="h5" component="div">
+                        {noteDetail.title}
+                    </Typography>
+                    <Typography variant="body2">Last Modified: {moment(noteDetail.timeLastModified).startOf('ss').fromNow() }</Typography>
+                </DialogTitle>
+                <Divider />
+                <DialogContent style={{ width: '500px', height: '300px' }}>
+                    {noteBodyEl}
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Close</Button>
+                    <Link to="/" className="dialog-close-btn">
+                        <Button onClick={handleClose}>Close</Button>
+                    </Link>
                 </DialogActions>
             </Dialog>
         </>
