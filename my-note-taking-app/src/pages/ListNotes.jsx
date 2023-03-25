f0cb0808b5b295c0d68e268df7b06c00656b1dfe
nathanlao <gla93@sfu.ca>
2023-03-25 16:22:27 -0700

    Using Grid for list of notes

--- my-note-taking-app/src/pages/ListNotes.jsx
+++ my-note-taking-app/src/pages/ListNotes.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
-import { List, Typography, ListItem, ListItemText, 
+import { Grid, List, Typography, ListItem, ListItemText, 
     ListItemIcon, IconButton, Divider, Snackbar, Alert } from "@mui/material"
 import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
@@ -31,36 +31,40 @@ export default function ListNotes({ notes, setNotes }) {
     // Map over note element with List into component
     const noteElements = notes.map((note) => {
         return  (
-            <List key={note.id} >
-                <ListItem secondaryAction={
-                        <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteNote(note.id)}>
-                            <DeleteForeverOutlinedIcon />
-                        </IconButton>
-                    }
-                    sx={{ py: 0.5 }}
-                    className="list-notes"
-                >
-                    <ListItemIcon>
-                        <EventNoteOutlinedIcon />
-                    </ListItemIcon>
-                    <Link to={`/${note.id}`} >
-                        <ListItemText 
-                            sx={{ marginLeft: -2 }}
-                            primaryTypographyProps={{ textTransform: "uppercase" }}
-                            primary={note.title} 
-                            secondary={`Created at: ${moment(note.timeCreated).startOf('ss').fromNow()}`}
-                        />
-                    </Link>
-                </ListItem>
-                <Divider component="li" />
-            </List>
+            <Grid item xs={12} sm={6} md={4} key={note.id}>
+                <List >
+                    <ListItem secondaryAction={
+                            <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteNote(note.id)}>
+                                <DeleteForeverOutlinedIcon />
+                            </IconButton>
+                        }
+                        sx={{ py: 0.5 }}
+                        className="list-notes"
+                    >
+                        <ListItemIcon>
+                            <EventNoteOutlinedIcon />
+                        </ListItemIcon>
+                        <Link to={`/${note.id}`} >
+                            <ListItemText 
+                                sx={{ marginLeft: -2 }}
+                                primaryTypographyProps={{ textTransform: "uppercase" }}
+                                primary={note.title} 
+                                secondary={`Created at: ${moment(note.timeCreated).startOf('ss').fromNow()}`}
+                            />
+                        </Link>
+                    </ListItem>
+                    <Divider component="li" />
+                </List>
+            </Grid>
         )
     })
 
     return (
         <section className="list-notes-container">
-            <Typography sx={{ mt: 0, mb: 2 }} variant="h5" component="div">SAVED NOTES:</Typography> 
-            {noteElements}
+            <Typography sx={{ mt: 0, mb: 2 }} variant="h5" component="div">YOUR NOTES:</Typography> 
+            <Grid container spacing={1}>
+                {noteElements}
+            </Grid>
             <Snackbar
                 open={openSnackbar}
                 onClose={() => setOpenSnackbar(false)}
