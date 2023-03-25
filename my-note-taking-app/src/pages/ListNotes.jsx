4b35656268965681d99dc84aa41d9d74e08acc32
nathanlao <gla93@sfu.ca>
2023-03-25 15:49:35 -0700

    Added snackbar and alart when delete notes

--- my-note-taking-app/src/pages/ListNotes.jsx
+++ my-note-taking-app/src/pages/ListNotes.jsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
-import { List, Typography, ListItem, ListItemText, ListItemIcon, IconButton, Divider } from "@mui/material"
+import { List, Typography, ListItem, ListItemText, 
+    ListItemIcon, IconButton, Divider, Snackbar, Alert } from "@mui/material"
 import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 import moment from "moment"
@@ -8,12 +9,17 @@ import axios from "axios"
 
 export default function ListNotes({ notes, setNotes }) {
 
+    const [openSnackbar, setOpenSnackbar] = useState(false)
+
     async function handleDeleteNote(id) {
         const confirmDelete = window.confirm("Are you sure you want to delete this note?");
         if (confirmDelete) {
             try {
                 const response = await axios.delete(`http://localhost:3001/api/notes/${id}`)
                 const data = response.data
+
+                setOpenSnackbar(true)
+
                  // update state of notes after successful delete request
                 setNotes(preNotes => preNotes.filter((note) => id !== note.id))
             } catch (err) {
@@ -53,6 +59,15 @@ export default function ListNotes({ notes, setNotes }) {
         <section className="list-notes-container">
             <Typography sx={{ mt: 0, mb: 2 }} variant="h5" component="div">SAVED NOTES:</Typography> 
             {noteElements}
+            <Snackbar
+                open={openSnackbar}
+                onClose={() => setOpenSnackbar(false)}
+                autoHideDuration={3000}
+            >
+                <Alert onClose={() => setOpenSnackbar(false)} severity="success" sx={{ width: '100%' }}>
+                    Successfully deleted your note!
+                </Alert>
+            </Snackbar>
         </section>
     )
 }
